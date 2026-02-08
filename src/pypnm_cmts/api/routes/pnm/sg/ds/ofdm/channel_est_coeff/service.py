ba54ed44517ec00ff2bda29d30de23e03efde61f
Maurice Garcia <mgarcia01752@outlook.com>
2026-02-07 21:35:07 -0700

    Ruff Cleanup

--- src/pypnm_cmts/api/routes/pnm/sg/ds/ofdm/channel_est_coeff/service.py
+++ src/pypnm_cmts/api/routes/pnm/sg/ds/ofdm/channel_est_coeff/service.py
@@ -16,7 +16,9 @@ from pypnm.api.routes.common.extended.common_messaging_service import (
     MessageResponseType,
 )
 from pypnm.api.routes.common.service.status_codes import ServiceStatusCode
-from pypnm.api.routes.docs.pnm.ds.ofdm.chan_est_coeff.service import CmDsOfdmChanEstCoefService
+from pypnm.api.routes.docs.pnm.ds.ofdm.chan_est_coeff.service import (
+    CmDsOfdmChanEstCoefService,
+)
 from pypnm.config.pnm_config_manager import PnmConfigManager
 from pypnm.docsis.cable_modem import CableModem
 from pypnm.lib.inet import Inet
